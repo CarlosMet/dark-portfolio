68726995066f75840654cfafac9362fc0eb426d3
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-09-27 02:11:32 -0500

    md fixes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client"
+import React, {useRef} from 'react'
 import TransitionEffect from '../components/TransitionEffect'
+import emailjs from '@emailjs/browser';
+import confetti from 'canvas-confetti';
 
 export default function Page() {
+    const ref = useRef()
+
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log(ref.current.mail.value)
+        if(!regexEmail.test(ref.current.mail.value)){  
+            alert('insert a valid mail direction please')          
+            return
+        }
+        if(ref.current.name.value === ''){
+            alert('Insert a valid name please')
+            return
+        }
+        
+        emailjs.sendForm('service_7ahixdt', 'template_hkx5fx6', ref.current, 'rRvVb2_HbgNg3wp9z')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+
+          confetti()
+        
+      };
+
   return (
-    <section className='w-screen h-screen flex flex-col lg:flex-row items-center gap-10 lg:gap-0 pt-24 md:pt-7 lg:pt-0 lg:justify-between'>
+    <section className='w-screen h-screen flex flex-col lg:flex-row items-center gap-10 md:gap-5 lg:gap-0 pt-24 md:pt-5 lg:pt-0 lg:justify-between'>
         <TransitionEffect />
         <img src="https://i.imgur.com/Mw1p9T4.png" className='absolute mix-blend-color-dodge right-0 bottom-0 md:w-7/12 -z-10' alt="explosion" />
         <div className='w-full lg:w-5/12'>
@@ -11,30 +42,30 @@ export default function Page() {
             <p className='text-slate-400 text-lg tracking-tight'></p>
         </div>
 
-        <form className='w-full lg:w-6/12' action="">
+        <form onSubmit={sendEmail} ref={ref} className='w-full lg:w-6/12' action="">
             <div className='flex w-full items-center justify-between'>
                 <div className='w-[48%]'>
                     <label className='text-slate-400' htmlFor="">Name</label><br />
-                    <input type="text" className='bg-white/5 focus:outline-none border border-slate-400 rounded-lg w-full px-2 lg:px-4 py-3 md:py-1 lg:py-3' />
+                    <input name='name' type="text" className='bg-white/5 focus:outline-none border border-slate-400 rounded-lg w-full px-2 lg:px-4 py-3 md:py-1 lg:py-3' />
                 </div>
                 <div className='w-[48%]'>
                     <label className='text-slate-400' htmlFor="">Lastname</label><br />
-                    <input type="text" className='bg-white/5 focus:outline-none border border-slate-400 rounded-lg w-full px-2 lg:px-4 py-3 md:py-1 lg:py-3' />
+                    <input name='lastname' type="text" className='bg-white/5 focus:outline-none border border-slate-400 rounded-lg w-full px-2 lg:px-4 py-3 md:py-1 lg:py-3' />
                 </div>
                 
             </div>
 
             <div className='mt-2'>
                 <label className='text-slate-400' htmlFor="">E-mail</label><br />
-                <input type="email" className='bg-white/5 focus:outline-none border border-slate-400 rounded-lg w-full px-2 lg:px-4 py-3 md:py-1 lg:py-3' />
+                <input name='mail' type="email" className='bg-white/5 focus:outline-none border border-slate-400 rounded-lg w-full px-2 lg:px-4 py-3 md:py-1 lg:py-3' />
             </div>
 
-            <div className='mt-3'>
+            <div className='mt-3 md:mt-2 lg:mt-3'>
                 <label className='text-slate-400' htmlFor="">Message</label><br />
-                <input type="text" className='bg-white/5 focus:outline-none border border-slate-400 rounded-lg w-full px-2 lg:px-4 py-2 h-24 md:h-20 lg:h-28' />
+                <input name='message' type="text" className='bg-white/5 focus:outline-none border border-slate-400 rounded-lg w-full px-2 lg:px-4 py-2 h-24 md:h-[72px] lg:h-28' />
             </div>
             
-            <button className='bg-transparent border-2 border-[#FB1F65] mt-6 w-44 mx-auto block py-2 md:py-1 lg:py-2 xl:py-3 rounded-3xl text-slate-300 font-bold text-xl' type="submit">
+            <button className='bg-transparent border-2 border-[#FB1F65] mt-6 md:mt-4 lg:mt-6 w-44 mx-auto block py-2 md:py-1 lg:py-2 xl:py-3 rounded-3xl text-slate-300 font-bold text-xl' type="submit">
                 Send
             </button>
             
